95572a3e1c8d12d3e43e415dd52b80d80f0ecca5
Nicolas Marquez <nicomars270@gmail.com>
2024-11-05 16:23:12 -0300

    Puntos

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -11,6 +11,7 @@ import VectorSource from 'ol/source/Vector';
 import VectorTileLayer from 'ol/layer/VectorTile';
 import { Style, Icon } from 'ol/style';
 import { applyStyle } from 'ol-mapbox-style';
+import { Punto } from '../interfaces/punto';
 
 @Component({
   selector: 'app-map',
@@ -20,6 +21,15 @@ import { applyStyle } from 'ol-mapbox-style';
 })
 export class MapComponent implements OnInit {
   map!: Map;
+
+  puntos: Punto[] = [
+    { latitud: -56.1645, longitud: -34.9011 }, // Punto central
+    { latitud: -56.1687, longitud: -34.892 },
+    { latitud: -56.1601, longitud: -34.8897 },
+    { latitud: -56.1663, longitud: -34.915 },
+    { latitud: -56.1618, longitud: -34.9213 },
+    { latitud: -56.1582, longitud: -34.8842 },
+  ];
 
   ngOnInit(): void {
     this.initializeMap();
@@ -33,15 +43,18 @@ export class MapComponent implements OnInit {
     // Configura la vista centrada en Montevideo
     const view = new View({
       center: fromLonLat([-56.1645, -34.9011]),
-      zoom: 10,
+      zoom: 13,
     });
 
     // Crea una capa vectorial vacía que se llenará con el estilo de Mapbox
     const mapboxLayer = new VectorTileLayer();
 
     // Aplica el estilo de Mapbox a la capa vectorial
-    await applyStyle(mapboxLayer, 'https://api.mapbox.com/styles/v1/mapbox/streets-v11?access_token=pk.eyJ1IjoibWFydGlnZGYiLCJhIjoiY20zNHM1a2FrMDJ6NTJrcHJqczljYXc5aCJ9.mWqbrFybjVe6OyoZBfaYZQ');
-    
+    await applyStyle(
+      mapboxLayer,
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11?access_token=pk.eyJ1IjoibWFydGlnZGYiLCJhIjoiY20zNHM1a2FrMDJ6NTJrcHJqczljYXc5aCJ9.mWqbrFybjVe6OyoZBfaYZQ'
+    );
+
     // Crea el mapa con las capas OSM y Mapbox
     this.map = new Map({
       target: 'map',
@@ -50,7 +63,9 @@ export class MapComponent implements OnInit {
     });
 
     // Agrega un punto en Montevideo
-    this.addPointGeometry([-56.1645, -34.9011]);
+    for (let punto of this.puntos) {
+      this.addPointGeometry([punto.latitud, punto.longitud]);
+    }
   }
 
   private addPointGeometry(coordinates: [number, number]): void {
@@ -68,7 +83,7 @@ export class MapComponent implements OnInit {
         }),
       })
     );
-    
+
     // Capa vectorial para mostrar el punto
     const vectorSource = new VectorSource({
       features: [pointFeature],
@@ -80,6 +95,5 @@ export class MapComponent implements OnInit {
 
     // Añade la capa vectorial al mapa
     this.map.addLayer(vectorLayer);
-    
   }
 }
